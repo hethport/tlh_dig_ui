c98277cba649def056d959b949fa7ac51fda141b
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2020-11-19 09:42:11 +0100

    fixed package.json

--- src/model/damages.ts
+++ src/model/damages.ts
@@ -1,22 +1,34 @@
 interface IDamage {
-    type: 'DeletionStart' | 'DeletionEnd' | 'LesionStart' | 'LesionEnd' | 'RasureStart' | 'RasureEnd';
+    type: 'Deletion' | 'Lesion' | 'Rasure' | 'Surplus' | 'Supplement';
+    position: 'Start' | 'End';
     symbol: string;
 }
 
-export const DeletionStart: IDamage = {type: 'DeletionStart', symbol: '['};
-export const DeletionEnd: IDamage = {type: 'DeletionEnd', symbol: ']'};
+export const DeletionStart: IDamage = {type: 'Deletion', position: 'Start', symbol: '['};
+export const DeletionEnd: IDamage = {type: 'Deletion', position: 'End', symbol: ']'};
 
-export const LesionStart: IDamage = {type: 'LesionStart', symbol: '('};
-export const LesionEnd: IDamage = {type: 'LesionEnd', symbol: ')'};
+export const LesionStart: IDamage = {type: 'Lesion', position: 'Start', symbol: '⸢'};
+export const LesionEnd: IDamage = {type: 'Lesion', position: 'End', symbol: '⸣'};
 
-export const RasureStart: IDamage = {type: 'RasureStart', symbol: '⸢'};
-export const RasureEnd: IDamage = {type: 'RasureEnd', symbol: '⸣'};
+export const RasureStart: IDamage = {type: 'Rasure', position: 'Start', symbol: '*'};
+export const RasureEnd: IDamage = {type: 'Rasure', position: 'End', symbol: '*'};
 
-export const allDamages = [DeletionStart, DeletionEnd, LesionStart, LesionEnd, RasureStart, RasureEnd];
-export type Damages =
-    typeof DeletionStart
-    | typeof DeletionEnd
-    | typeof LesionStart
-    | typeof LesionEnd
-    | typeof RasureStart
-    | typeof RasureEnd;
+export const SurplusStart: IDamage = {type: 'Surplus', position: 'Start', symbol: '〈〈'};
+export const SurplusEnd: IDamage = {type: 'Surplus', position: 'End', symbol: '〉〉'};
+
+export const SupplementStart: IDamage = {type: 'Supplement', position: 'Start', symbol: '〈'};
+export const SupplementEnd: IDamage = {type: 'Supplement', position: 'End', symbol: '〉'};
+
+export const allDamages = [
+    DeletionStart, DeletionEnd,
+    LesionStart, LesionEnd,
+    RasureStart, RasureEnd,
+    SurplusStart, SurplusEnd,
+    SupplementStart, SupplementEnd
+];
+
+export type Damages = typeof DeletionStart | typeof DeletionEnd
+    | typeof LesionStart | typeof LesionEnd
+    | typeof RasureStart | typeof RasureEnd
+    | typeof SurplusStart | typeof SurplusEnd
+    | typeof SupplementStart | typeof SupplementStart;
