e62de651e600cbb52247c6d6b2efa37d4a60d85f
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2021-02-23 09:55:06 +0100

    client package updates

--- src/forms/schemas.ts
+++ src/forms/schemas.ts
@@ -1,100 +1,65 @@
+import * as yup from "yup";
 import {
-    array as yupArray,
-    boolean as yupBoolean,
-    mixed as yupMixed,
-    number as yupNumber,
-    object as yupObject,
-    ObjectSchema,
-    string as yupString
-} from "yup";
-import {
-    CreateManuscriptMutationVariables,
-    LoginMutationVariables,
-    ManuscriptIdentifierInput,
-    ManuscriptIdentifierType,
-    PalaeographicClassification,
-    UserInput
+  CreateManuscriptMutationVariables,
+  LoginMutationVariables,
+  ManuscriptIdentifierInput,
+  ManuscriptIdentifierType,
+  PalaeographicClassification,
+  UserInput
 } from "../generated/graphql";
 
-export const registerSchema = yupObject<UserInput>()
-    .shape({
-        // TODO: test password === passwordRepeat
-        username: yupString()
-            .min(4)
-            .max(50)
-            .required(),
-        password: yupString()
-            .min(4)
-            .max(50)
-            .required(),
-        passwordRepeat: yupString()
-            .min(4)
-            .max(50)
-            .required(),
-        email: yupString()
-            .email()
-            .required(),
-        name: yupString()
-            .required(),
-        affiliation: yupString()
-            .notRequired()
-    })
-    .required();
+export const registerSchema: yup.SchemaOf<UserInput> = yup.object()
+  .shape({
+    // TODO: test password === passwordRepeat
+    username: yup.string().min(4).max(50).required(),
+    password: yup.string().min(4).max(50).required(),
+    passwordRepeat: yup.string().min(4).max(50).required(),
+    email: yup.string().email().required(),
+    name: yup.string().required(),
+    affiliation: yup.string().notRequired()
+  })
+  .required();
 
-export const loginSchema: ObjectSchema<LoginMutationVariables> = yupObject<LoginMutationVariables>()
-    .shape({
-        username: yupString()
-            .min(4)
-            .max(50)
-            .required(),
-        password: yupString()
-            .min(4)
-            .max(50)
-            .required()
-    })
-    .required();
+export const loginSchema: yup.SchemaOf<LoginMutationVariables> = yup.object()
+  .shape({
+    username: yup.string().min(4).max(50).required(),
+    password: yup.string().min(4).max(50).required()
+  })
+  .required();
 
-const manuscriptIdentifierSchema: ObjectSchema<ManuscriptIdentifierInput> = yupObject<ManuscriptIdentifierInput>()
-    .shape({
-        identifier: yupString()
-            .required(),
-        identifierType: yupMixed<ManuscriptIdentifierType>()
-            .oneOf([
-                ManuscriptIdentifierType.CollectionNumber,
-                ManuscriptIdentifierType.ExcavationNumber,
-                ManuscriptIdentifierType.PublicationShortReference
-            ] as const)
-            .required()
-    })
-    .required();
+const manuscriptIdentifierSchema: yup.SchemaOf<ManuscriptIdentifierInput> = yup.object()
+  .shape({
+    identifier: yup.string().required(),
+    identifierType: yup.mixed()
+      .oneOf([
+        ManuscriptIdentifierType.CollectionNumber,
+        ManuscriptIdentifierType.ExcavationNumber,
+        ManuscriptIdentifierType.PublicationShortReference
+      ])
+      .required()
+  })
+  .required();
 
-export const manuscriptSchema: ObjectSchema<CreateManuscriptMutationVariables> = yupObject<CreateManuscriptMutationVariables>()
-    .shape({
-        // TODO: other fields!
-        mainIdentifier: manuscriptIdentifierSchema
-            .required(),
-        otherIdentifiers: yupArray(manuscriptIdentifierSchema)
-            .notRequired(),
-        palaeographicClassification: yupMixed<PalaeographicClassification>()
-            .oneOf([
-                PalaeographicClassification.Unclassified,
-                PalaeographicClassification.AssyroMittanianScript,
-                PalaeographicClassification.LateNewScript,
-                PalaeographicClassification.MiddleAssyrianScript,
-                PalaeographicClassification.MiddleBabylonianScript,
-                PalaeographicClassification.MiddleScript,
-                PalaeographicClassification.NewScript,
-                PalaeographicClassification.OldAssyrianScript,
-                PalaeographicClassification.OldScript
-            ] as const)
-            .required(),
-        palaeographicClassificationSure: yupBoolean()
-            .required(),
-        cthClassification: yupNumber()
-            .notRequired(),
-        provenance: yupString()
-            .notRequired(),
-        bibliography: yupString()
-            .notRequired()
-    })
-    .required();
+export const manuscriptSchema: yup.SchemaOf<CreateManuscriptMutationVariables> = yup.object()
+  .shape({
+    // TODO: other fields!
+    mainIdentifier: manuscriptIdentifierSchema.required(),
+    otherIdentifiers: yup.array(manuscriptIdentifierSchema).notRequired(),
+    palaeographicClassification: yup.mixed<PalaeographicClassification>()
+      .oneOf([
+        PalaeographicClassification.Unclassified,
+        PalaeographicClassification.AssyroMittanianScript,
+        PalaeographicClassification.LateNewScript,
+        PalaeographicClassification.MiddleAssyrianScript,
+        PalaeographicClassification.MiddleBabylonianScript,
+        PalaeographicClassification.MiddleScript,
+        PalaeographicClassification.NewScript,
+        PalaeographicClassification.OldAssyrianScript,
+        PalaeographicClassification.OldScript
+      ])
+      .required(),
+    palaeographicClassificationSure: yup.boolean().required(),
+    cthClassification: yup.number().notRequired(),
+    provenance: yup.string().notRequired(), bibliography: yup.string().notRequired()
+  })
+  .required();
